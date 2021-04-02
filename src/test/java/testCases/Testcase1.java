2b79cc31058113617d241495e6177f06a3b51867
shubham <kumarshubham2994@gmail.com>
2021-04-02 10:32:00 +0530

    logs added

--- src/test/java/testCases/Testcase1.java
+++ src/test/java/testCases/Testcase1.java
@@ -2,6 +2,9 @@ package testCases;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
@@ -10,6 +13,7 @@ import pageObjects.LandingPage;
 import resources.BaseTest;
 
 public class Testcase1 extends BaseTest {
+	private static Logger log = LogManager.getLogger(BaseTest.class.getName());
 
 	@Test
 	public void flipkart() throws InterruptedException {
@@ -17,20 +21,29 @@ public class Testcase1 extends BaseTest {
 		LandingPage objLand = new LandingPage(driver);
 		FilterPage objF = new FilterPage(driver);
 
+		log.info("Entering Email Id");
 		objLand.getEmailId().sendKeys(prop.getProperty("emailId"));
+		log.info("Entering password");
 		objLand.getPassword().sendKeys(prop.getProperty("password"));
+		log.info("Clicking on Login Button");
 		objLand.getLoginButton().click();
 		Thread.sleep(2000);
+		log.info("Search for item: " + prop.getProperty("item"));
 		objLand.getSearchBox().sendKeys(prop.getProperty("item") + Keys.ENTER);
 		Thread.sleep(5000);
+		log.info("Selecting Mobile Filter");
 		objF.getMobileFilter().click();
 		Thread.sleep(3000);
+		log.info("Selecting Samsung Filter");
 		objF.getSamsungFilter().click();
 		Thread.sleep(2000);
+		log.info("Selecting Flikart Assured Filter");
 		objF.getAssuredFilter().click();
 		Thread.sleep(2000);
+		log.info("Selecting High to Low Price Filter");
 		objF.getHighToLowFilter().click();
 		Thread.sleep(4000);
+		log.info("Getting the item details");
 		List<String> webList = new ArrayList<String>();
 		for (int i = 0; i < objF.getItems().size(); i++) {
 
@@ -40,6 +53,7 @@ public class Testcase1 extends BaseTest {
 			webList.add("\n");
 		}
 		System.out.println(webList);
+		log.info(webList);
 
 	}
 
